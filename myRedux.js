f5bf0c88255d84b73f27e6d4e38247a5f34c3e06
hyang <1348261663@qq.com>
2019-06-23 22:13:16 +0800

    Update myRedux.js

--- myRedux.js
+++ myRedux.js
@@ -1,1 +1,56 @@
 
+//createStore
+function createStore(reducer, initialState) {
+ // currentState就是那个数据
+ let currentState = initialState;
+ let listener = () => {};
+ 
+ function getState() {
+   return currentState;
+ }
+ function dispatch(action) {
+   currentState = reducer(currentState, action); // 更新数据
+   listener(); // 执行订阅函数
+   return action;
+ }
+ function subscribe(newListener) {
+   listener = newListener;
+   // 取消订阅函数
+   return function unsubscribe() {
+    listener = () => {};
+   };
+ }
+ return {
+   getState: getState,
+   dispatch: dispatch,
+   subscribe: subscribe
+ };
+};
+//action func
+function addTodo(text){
+	return {type: 'ADD_TODO', text: text}
+};
+
+//reducer
+function todos(state=[], action) {
+  return [
+    ...state,
+    {
+      text: action.text,
+      completed: false
+    }
+  ]
+}
+function todoApp(state={}, action) {
+  return {
+    todos: todos(state.todos, action)
+  }
+}
+ 
+const store = createStore(todoApp,{todos:[{text: 'hello redux', completed: false}]});
+console.log(store.getState()); // 获取数据
+const unsubscribe = store.subscribe(()=>{
+  console.log(store.getState())
+}); // 注册订阅函数
+
+store.dispatch(addTodo('hello word'));
