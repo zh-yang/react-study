de39a1928480ea7b6ddef125b339194b2573580e
yzhih <yzhih@servyou.local>
2019-06-25 19:58:18 +0800

    redux

---
+++ my-app/src/counter/reducer.js
@@ -0,0 +1,22 @@
+import { createStore } from 'redux';
+
+/**
+ * 这是一个 reducer，形式为 (state, action) => state 的纯函数。
+ * 描述了 action 如何把 state 转变成下一个 state。
+ * 
+ */ 
+function counter(state = 0, action) {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
+}
+
+//创建 Redux store 来存放应用的状态
+//API 是 { subscribe, dispatch, getState }
+let store = createStore(counter);
+
